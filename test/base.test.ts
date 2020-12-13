5b5ca675c2029dc2e23176900ff81fa54f89b81f
Denis Churbanov <4urbanoff@gmail.com>
2020-12-13 14:28:22 +0300

    update tests

--- test/base.test.ts
+++ test/base.test.ts
@@ -4,20 +4,21 @@
 
 import { watch } from '@fract/core'
 import { Pathname } from '../src/pathname'
+import { redirect } from '../src/utils'
 
 const pathname = new Pathname()
 const mock = jest.fn()
 const dispose = watch(pathname, mock)
 
-it('initial pathname to be "/"', async () => {
+it('initial pathname to be "/"', () => {
     expect(mock).lastCalledWith('/')
 })
 
-it('redirect to "/test"', async () => {
-    pathname.redirect('/test')
+it('redirect to "/test"', () => {
+    redirect('/test')
     expect(mock).lastCalledWith('/test')
 })
 
-it('should remove listener when destroy', async () => {
+it('should remove listener when destroy', () => {
     dispose()
 })
--- test/exports.test.ts
+++ test/exports.test.ts
@@ -1,6 +1,8 @@
-import { Pathname } from '../src/index'
+import { Pathname, redirect } from '../src/index'
 import { Pathname as _Pathname } from '../src/pathname'
+import { redirect as _redirect } from '../src/utils'
 
 it('exports', () => {
     expect(Pathname).toBe(_Pathname)
+    expect(redirect).toBe(_redirect)
 })
